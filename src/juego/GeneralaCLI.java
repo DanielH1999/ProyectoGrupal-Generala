758486a12578a823c5ed1f5a725e1d8786e60348
Daniel Gaspar Arroyo Herrero <daniel1999275@hotmail.com>
2021-11-27 23:33:57 -0300

    ToDo 4 completo
    correccion de problemas previstos, adicion de comentarios, remocion de variable cantDados, reemplazada por dados.length en casi todas sus instancias

--- src/juego/GeneralaCLI.java
+++ src/juego/GeneralaCLI.java
@@ -20,9 +20,6 @@ public class GeneralaCLI
 		
 		int cantTurnos = 1; //Cantidad de turnos por jugador (DEFAULT: 3)
 		
-		int cantDados = 5; //Cantidad de dados
-		
-		
 		String[] jugadores = new String[maxJugadores]; //Array de jugadores
 		
 		jugadores = averiguarJugadores(maxJugadores); //Invocacion del metodo que averigua los nombres de los jugadores y los guarda en jugadores
@@ -46,22 +43,22 @@ public class GeneralaCLI
 		//INICIO DEL JUEGO
 		for (int jugador = 0; jugador < jugadores.length; jugador++) //llevar cuentas de jugador actual
 		{
+			//tirar dados por primera vez para cada jugador
+			int[] dados = tirarDados(5); //Array de dados = return del metodo que pone valores al azar
+			
 			for (int turno = 0; turno < cantTurnos; turno++) //llevar cuentas de turno por jugador
 			{
 				//llevar cuentas de las jugadas que faltan hacer
 				//-> Informar el ganador cuando se hayan terminado las jugadas posibles o alguien consiga GENERALA SERVIDA
 				
+
+				mostrarDados(dados);//Mostrar los dados en el array
+
+				//ToDo 4 (HECHO):
+				cambiarDados(dados, turno);//-> Hacerlo hasta que se terminen sus turno
 				
-				int[] dados = tirarDados(cantDados); //Array de los 5 dados = invocacion del metodo que pone valores al azar
-
-				System.out.println(Arrays.toString(dados));//Mostrar los dados en el array
-
-				//ToDo 4 (tomado por Alejandra):
-				//Preguntarle al usuario si quiere cambiar algun dado
-				//Dejar que el usuario seleccione los dados que quiere cambiar
-				//Volver a tirar los dados a cambiar
-				//-> Hacerlo hasta que se terminen sus turno
-
+				Arrays.sort(dados);
+				
 				//ToDo 5:
 				//Revisar la lista de dados y comparar con las posibles jugadas especiales
 				//Informarle al usuario donde puede anotar puntos
@@ -98,17 +95,119 @@ public class GeneralaCLI
 		return Jugadores; //Devolver la lista de jugadores al metodo main()
 	}
 	
-	private static int[] tirarDados(int cantDados) //metodo que toma como parametro int cantDados y devuelve int[] dados
+	private static int[] tirarDados(int cantDados) //metodo que toma como parametro int dados.length y devuelve int[] dados
 	{	
 		Random azar = new Random(); //Una clase del paquete java.util que genera numeros pseudo-aleatorios
 		
 		int[] dados = new int[cantDados]; //Declaro un array para poner los dados
 		
-		for (int i = 0; i < cantDados; i++) //Iterar por la cantidad de dados del array
+		for (int i = 0; i < dados.length; i++) //Iterar por la cantidad de dados del array
 		{
 			dados[i] = 1 + azar.nextInt(6); //Poner un valor aleatorio en el dado actual
 		}
 		return dados; //Devuelvo el int[] dados
+	}
+
+	private static void mostrarDados(int[] dados) {
+		System.out.print("[");
+		
+		for (int j = 0; j < dados.length; j++)
+		{
+			if (j > 0 && j < dados.length) //si no es el valor inicial o final
+			{
+				System.out.print(", ");
+			}
+			
+			if (dados[j] == 7) //si el dado fue marcado para un cambio
+			{
+				System.out.print("x");
+			}
+			else
+			{
+				System.out.print(dados[j]);
+			}
+		}
+		
+		System.out.println("]");
+	}
+
+	private static void cambiarDados(int[] dados, int turno)
+	{
+		Scanner leer = new Scanner(System.in);
+		Random azar = new Random();
+
+		//Preguntarle al usuario si quiere cambiar algun dado
+		System.out.println("¿Quiere cambiar algún dado? (S/N)");
+		//tomar un si o un no y en base a eso ejecutar los siguientes pasos
+
+		String respuesta; //tomar respuesta
+		boolean cambiar = false; //si el usuario quiere cambiar dados o no
+
+		do
+		{
+			respuesta = leer.next();
+
+			if (respuesta.equalsIgnoreCase("S"))
+			{
+				cambiar = true;
+			}
+			else if (respuesta.equalsIgnoreCase("N"))
+			{
+				cambiar = false;
+			}
+		}
+		while (!(respuesta.equalsIgnoreCase("S")) && !(respuesta.equalsIgnoreCase("N")));
+
+		if (cambiar)
+		{
+			//repetir la toma de elecciones hasta alcanzar cantidad de dados o eleccion = 0
+			int eleccion = 1;
+
+			while (eleccion != 0)
+			{
+				//Dejar que el usuario seleccione los dados que quiere cambiar
+
+
+				System.out.println("Seleccione el dado a cambiar [1~5], [0] para terminar de cambiar");
+
+				do
+				{	
+					eleccion = leer.nextInt();
+
+					if (eleccion > 5 || eleccion < 0)
+					{
+						System.out.println("ese dado no existe, ingrese 0 para dejar de cambiar");
+					}
+				}
+				while (eleccion > 5 || eleccion < 0);
+
+				//si eleccion es 0, dejar de marcar dados
+				if (eleccion == 0)
+				{
+					break;
+				}
+
+				//marcar dados para volver a tirar
+				dados[eleccion -1] = 7;
+
+				//mostrar los dados cambiando
+				mostrarDados(dados);
+			}
+
+			//Iterar por la cantidad de dados del array
+			for (int i = 0; i < dados.length; i++)
+			{
+				//Volver a tirar los dados a cambiar
+				if (dados[i] == 7)
+				{
+					dados[i] = 1 + azar.nextInt(6);
+				}
+			}
+
+			turno++;
+
+			mostrarDados(dados);
+		}
 	}
 	
 	private int/*<-tipo de dato que devuelve*/ metodoDeEjemplo(int parametro, int otroParametro)
